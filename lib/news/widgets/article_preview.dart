3b8edb60a08f9311b1528c80d6ef108baa9f71f4
MatzeMarcel <marcel.garus@gmail.com>
2019-05-16 00:02:56 +0200

    feat(news): add dummy widgets to article preview

--- lib/news/widgets/article_preview.dart
+++ lib/news/widgets/article_preview.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../model.dart';
+import 'author.dart';
+import 'headline.dart';
+import 'section.dart';
 
 class ArticlePreview extends StatelessWidget {
   ArticlePreview({
@@ -18,8 +21,22 @@ class ArticlePreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      elevation: 8,
+      elevation: 2,
       borderRadius: BorderRadius.circular(8),
+      child: Container(
+        height: 300,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 8),
+            Section(content: article.section),
+            SizedBox(height: 8),
+            AuthorView(author: article.author),
+            SizedBox(height: 8),
+            Headline(title: article.title, published: article.published),
+          ],
+        ),
+      ),
     );
   }
 }
