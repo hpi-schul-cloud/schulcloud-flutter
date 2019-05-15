737f0fc9528ef5436adcc923c87fe62fd9448668
MatzeMarcel <marcel.garus@gmail.com>
2019-05-16 00:01:40 +0200

    feat(news): added section banner

---
+++ lib/news/widgets/section.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Section extends StatelessWidget {
+  Section({
+    @required this.content,
+  }) : assert(content != null);
+
+  final String content;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: SectionClipper(),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [Color(0xff200e32), Color(0xff6d1541)],
+          ),
+        ),
+        padding: const EdgeInsets.all(8) + const EdgeInsets.only(right: 16),
+        child: Text(content, style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
+
+class SectionClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var width = size.width;
+    var height = size.height;
+    var cutIn = 0.4 * size.shortestSide;
+    var controlPoint = width - 0.8 * cutIn;
+    return Path()
+      ..lineTo(width - cutIn, 0)
+      ..cubicTo(
+          controlPoint, 0, controlPoint, 0, width - 0.6 * cutIn, 0.2 * height)
+      ..lineTo(width, height)
+      ..lineTo(0, height)
+      ..close();
+  }
+
+  @override
+  bool shouldReclip(SectionClipper oldClipper) => true;
+}
