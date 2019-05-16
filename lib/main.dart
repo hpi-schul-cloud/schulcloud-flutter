e9b887736d06e2cd8e29de3fddec72aa6acda29c
MatzeMarcel <marcel.garus@gmail.com>
2019-05-17 01:49:39 +0200

    feat(app): use fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,20 @@ import 'news/news.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Schulcloud',
-      theme: ThemeData(primarySwatch: Colors.orange),
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        fontFamily: 'PT Sans Narrow',
+        textTheme: TextTheme(
+          title: TextStyle(fontWeight: FontWeight.bold),
+          body2: TextStyle(fontSize: 20),
+          display1: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+          display2: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+        ),
+      ),
       home: NewsScreen(),
     );
   }
