27f965261bda8c088233caced6c4241b75bbe0a0
MatzeMarcel <marcel.garus@gmail.com>
2019-05-17 01:50:31 +0200

    feat(news): display author picture

--- lib/news/widgets/author.dart
+++ lib/news/widgets/author.dart
@@ -14,12 +14,29 @@ class AuthorView extends StatelessWidget {
     return Stack(
       children: <Widget>[
         Container(
-          color: Colors.white,
-          child: Text(author.name),
+          height: 56,
+          alignment: Alignment.centerLeft,
+          child: Container(
+            color: Colors.white,
+            margin: const EdgeInsets.only(left: 28),
+            padding: const EdgeInsets.fromLTRB(40, 4, 8, 4),
+            child: Text(
+              'von ${author.name}',
+              style: Theme.of(context).textTheme.caption.copyWith(fontSize: 16),
+            ),
+          ),
         ),
-        CircleAvatar(
-          backgroundColor: Colors.grey,
-          radius: 24,
+        Container(
+          height: 56,
+          width: 56,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.grey, width: 1.5),
+          ),
+          child: CircleAvatar(
+            backgroundColor: Colors.grey,
+            backgroundImage: NetworkImage(author.photoUrl),
+          ),
         ),
       ],
     );
