92d0690d10f5ef1deb005af83240320d5c41dc7c
MatzeMarcel <marcel.garus@gmail.com>
2019-05-17 01:51:44 +0200

    feat(news): layout widgets in right order

--- lib/news/widgets/article_preview.dart
+++ lib/news/widgets/article_preview.dart
@@ -23,17 +23,19 @@ class ArticlePreview extends StatelessWidget {
     return Material(
       elevation: 2,
       borderRadius: BorderRadius.circular(8),
+      color: Colors.black12,
       child: Container(
         height: 300,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 8),
             Section(content: article.section),
-            SizedBox(height: 8),
-            AuthorView(author: article.author),
-            SizedBox(height: 8),
             Headline(title: article.title, published: article.published),
+            Transform.translate(
+              offset: Offset(28, -13),
+              child: AuthorView(author: article.author),
+            ),
+            SizedBox(height: 8),
           ],
         ),
       ),
