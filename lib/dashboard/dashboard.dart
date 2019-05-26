2aff49d8782b1672ce0188bac976f93851a92e72
MatzeMarcel <marcel.garus@gmail.com>
2019-05-26 21:50:11 +0200

    feat(dashboard): add dashboard

---
+++ lib/dashboard/dashboard.dart
@@ -0,0 +1,1 @@
+export 'widgets/dashboard_screen.dart';
---
+++ lib/dashboard/widgets/dashboard_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'package:schulcloud/news/news.dart';
+
+import 'menu.dart';
+
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  void _showMenu() {
+    showModalBottomSheet(context: context, builder: (context) => Menu());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NewsScreen(),
+      bottomNavigationBar: _buildBottomAppBar(),
+    );
+  }
+
+  Widget _buildBottomAppBar() {
+    return BottomAppBar(
+      color: Theme.of(context).primaryColor,
+      elevation: 6,
+      child: Container(
+        height: 56,
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.menu, color: Colors.white),
+              onPressed: _showMenu,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
