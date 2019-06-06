b512e17253c515b3a205e3d665a013f1d5a049b2
Marcel Garus <marcel.garus@gmail.com>
2019-06-06 22:16:47 +0200

    feat(news): add AuthorDto

---
+++ lib/news/data/author.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import 'package:schulcloud/core/data.dart';
+
+part 'author.g.dart';
+
+@JsonSerializable()
+@immutable
+class AuthorDto extends Dto<AuthorDto> {
+  final Id<AuthorDto> id;
+  final String name;
+  final String photoUrl;
+
+  const AuthorDto({
+    @required this.id,
+    @required this.name,
+    this.photoUrl,
+  }) : assert(name != null);
+
+  factory AuthorDto.fromJson(Map<String, dynamic> json) =>
+      _$AuthorDtoFromJson(json);
+  Map<String, dynamic> toJson() => _$AuthorDtoToJson(this);
+}
