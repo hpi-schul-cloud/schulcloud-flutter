2670425a4782a81de68961a8c05404b4d34e25ba
Marcel Garus <marcel.garus@gmail.com>
2019-06-11 01:12:18 +0200

    refactor(core): merge repositories base classes into one class (WIP)

--- lib/core/data/data.dart
+++ lib/core/data/data.dart
@@ -12,6 +12,8 @@ class Id<T> {
 
   bool matches(String id) => this.id == id;
 
+  Id<OtherType> cast<OtherType>() => Id<OtherType>(id);
+
   factory Id.fromJson(Map<String, dynamic> json) => Id(json['id']);
   Map<String, dynamic> toJson() => {'id': id};
 }
@@ -34,14 +36,64 @@ abstract class Serializer<T> {
 class NotLoadedYetError {}
 
 /// Something that can fetch items. Always returns a stream of items.
+///
+/// If [isFinite] is true, the fetchAll() method should be implemented.
+/// If [isMutable] is true, the update() method should be implemented.
+///
+/// For convenience, you can provide a source repository. It will be disposed
+/// when this one gets disposed and if not specified otherwise, this repository
+/// inherits the [isFinite] and [isMutable] properties from the source.
+///
+/// When overriding methods, they should not call super.
 abstract class Repository<Item> {
-  const Repository();
+  // Reasons why this is a kind of monolithic class instead of splitting it up
+  // into smaller classes like a MutableRepository and FiniteRepository etc.:
+  //
+  // When implementing repositories, they often accept a source repository and
+  // their [isFinite] and [isMutable] properties depend on the source
+  // repository's properties. So you would have to do many manual checks.
+  //
+  // "But I'm losing my type safety!" Well, the pattern observed using a modular
+  // architecture was a lot of unsafe typecasting similar to the following:
+  // ```dart
+  // bool get isFinite => source.isFinite;
+  // ...
+  // if (isFinite) {
+  //   final source = this.source as FiniteRepository;
+  // }
+  // ```
 
-  bool get isFinite => this is FiniteRepository;
-  bool get isMutable => this is MutableRepository;
+  /// An optional source repository.
+  final Repository source;
 
+  /// Whether this repository is finite. If set to true, the [fetchAll] method
+  /// should be overriden and not call [super.fetchAll()].
+  bool get isFinite => _isFinite;
+  bool _isFinite;
+
+  /// Whether this repository is mutable. If set to true, the [update] method
+  /// should be overridden and not call [super.update()];
+  bool get isMutable => _isMutable;
+  bool _isMutable;
+
+  /// Constructor for a repository.
+  ///
+  /// By default, it's not mutable.
+  /// If a [source] repository is provided, finitability and mutability are
+  /// equal to the [source]'s, unless overridden here.
+  Repository({
+    this.source,
+    bool isFinite,
+    bool isMutable,
+  }) {
+    _isMutable = isMutable ?? source?.isMutable ?? false;
+    _isFinite = isFinite ?? source?.isFinite ?? false;
+  }
+
+  /// Fetches a single item with the given [id].
   Stream<Item> fetch(Id<Item> id);
 
+  /// Fetches multiple items with the given [ids].
   Stream<Iterable<Item>> fetchMultiple(Iterable<Id<Item>> ids) {
     Iterable<Stream<Item>> allStreams = ids.map(fetch);
 
@@ -59,29 +111,54 @@ abstract class Repository<Item> {
 
     return controller.stream;
   }
+
+  /// Fetches all items. May only be called if this [isFinite].
+  Stream<Iterable<Item>> fetchAll() {
+    if (isFinite) {
+      throw UnimplementedError(
+          "fetchAll was called on repository $this. It's finite, so that "
+          "should be possible. Make sure you implement the fetchAll method and "
+          "don't call the superclass's fetchAll method.");
+    } else {
+      throw UnsupportedError(
+          "fetchAll was called on repository $this, altough it's not finite. "
+          "Don't do that.");
+    }
+  }
+
+  /// Updates an item. May only be called if this [isMutable].
+  Future<void> update(Id<Item> id, Item value) {
+    if (isMutable) {
+      throw UnimplementedError(
+          "update was called on repository $this. It's mutable, so that "
+          "should be possible. Make sure you implement the update method and "
+          "don't call the superclass's update method.");
+    } else {
+      throw UnsupportedError(
+          "update was called on repository $this, altough it's not mutable. "
+          "Don't do that.");
+    }
+  }
+
+  /// Disposes the repository.
+  void dispose() => source?.dispose();
 }
 
-/// A repository with a finite amount of items. That means you can fetch all
-/// data, if you want to.
-/// Of course, all data sources are finite but the ones that are seemingly
-/// infinite to the client (like, authors of news articles) should not implement
-/// this class.
-abstract class FiniteRepository<Item> extends Repository<Item> {
-  const FiniteRepository();
+mixin SourceRepository<RepositoryType extends Repository<SourceItem>,
+    SourceItem> on Repository {
+  RepositoryType source;
 
-  Stream<Iterable<Item>> fetchAll();
-}
+  void useSource(RepositoryType source) {
+    assert(source != null);
+    this.source = source;
+  }
 
-/// A repository with mutable items. Provides a method to update them.
-abstract class MutableRepository<Item> extends Repository<Item> {
-  const MutableRepository();
+  @override
+  bool get isMutable => source.isMutable;
 
-  Future<void> update(Id<Item> id, Item value);
-}
+  @override
+  bool get isFinite => source.isFinite;
 
-/// A convenience class that represents a repository that's both finite and
-/// mutable.
-abstract class FiniteMutableRepository<Item> extends Repository<Item>
-    implements FiniteRepository<Item>, MutableRepository<Item> {
-  const FiniteMutableRepository();
+  @override
+  void dispose() => source.dispose();
 }
