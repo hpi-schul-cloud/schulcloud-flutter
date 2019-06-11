e2d4f35b5edbc236245ecca3420bd56578c3c638
Marcel Garus <marcel.garus@gmail.com>
2019-06-12 00:46:42 +0200

    refactor(core): move entity and json to data folder, remove dto (WIP)

--- lib/core/data/entity.dart
+++ lib/core/data/entity.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 
-/// Identifier that uniquely identifies a DTO and an entity among others of the
-/// same type.
+/// Identifier that uniquely identifies an object among others of the same type.
 @immutable
 class Id<T> {
   final String id;
@@ -14,4 +13,12 @@ class Id<T> {
 
   factory Id.fromJson(Map<String, dynamic> json) => Id(json['id']);
   Map<String, dynamic> toJson() => {'id': id};
+}
+
+/// A special kind of object that also carries its id.
+@immutable
+class Entity<T extends Entity<T>> {
+  final Id<T> id;
+
+  const Entity(this.id) : assert(id != null);
 }
