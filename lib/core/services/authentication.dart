a9542674b29957d89a5d5cbfbb9a9bb75cbc1924
Marcel Garus <marcel.garus@gmail.com>
2019-06-23 03:07:26 +0200

    feat(core): actually call API login

--- lib/core/services/authentication.dart
+++ lib/core/services/authentication.dart
@@ -3,34 +3,44 @@ import 'package:schulcloud/core/data.dart';
 
 import 'api.dart';
 
-class AuthenticationService {
-  ApiService api;
+class InvalidEmailSyntaxError {}
 
-  Repository<String> repo = SharedPreferences(
+class InvalidPasswordSyntaxError {}
+
+class AuthenticationService {
+  static const emailRegExp =
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
+
+  ApiService _api;
+
+  Repository<String> _repo = SharedPreferences(
     keyPrefix: 'authentication',
   );
-  static const userId = Id<String>('user');
+  static const emailId = Id<String>('email');
   static const passwordId = Id<String>('password');
+  static const accessTokenId = Id<String>('accessToken');
 
-  Stream<String> get user => repo.fetch(userId);
-  Stream<String> get password => repo.fetch(passwordId);
+  Stream<String> get email => _repo.fetch(emailId);
+  Stream<String> get password => _repo.fetch(passwordId);
+  Stream<String> get accessToken => _repo.fetch(accessTokenId);
 
-  AuthenticationService({@required this.api});
+  AuthenticationService({@required ApiService api}) : this._api = api;
 
-  Future<bool> login(String user, String password) async {
-    // Save the user.
-    repo.update(userId, user);
-
-    // Try to log in with the supplied username and password.
-    try {
-      await api.login(user, password);
-    } catch (e) {
-      // TODO: rethrow network issue errors
-      return false;
+  Future<void> login(String email, String password) async {
+    if (!RegExp(emailRegExp).hasMatch(email)) {
+      throw InvalidEmailSyntaxError();
     }
 
-    // Only if the login succeeded, also save the password.
-    repo.update(passwordId, password);
-    return true;
+    _repo.update(emailId, email);
+
+    if (password.isEmpty) {
+      throw InvalidPasswordSyntaxError();
+    }
+
+    // The login may throw an exception if it wasn't successful.
+    String accessToken = await _api.login(email, password);
+
+    _repo.update(passwordId, password);
+    _repo.update(accessTokenId, accessToken);
   }
 }
