b9fbc16ccdc5bac1aa57a9ee2b5d6bc2b9153e10
Marcel Garus <marcel.garus@gmail.com>
2019-06-26 00:24:47 +0200

    refactor(login): simplify button and add more states, displaying errors to the user appropriately

--- lib/login/widgets/button.dart
+++ lib/login/widgets/button.dart
@@ -1,49 +1,28 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 /// A button that can morph into a loading spinner.
 ///
 /// This button can be given a child to display. The passed [onPressed] callback
-/// is called if the button is pressed. In contrast to the normal built-in
-/// button, this button morphs into a loading spinner if the passed callback is
-/// asynchronous (if it returns a [Future]).
-/// If the future throws an error, the button stops spinning (so the user can
-/// try again) and the [onError] callback is called with the caught error.
-/// Otherwise the [onSuccess] callback is called with the result.
-class Button<T> extends StatefulWidget {
+/// is called if the button is pressed.
+/// If [isLoading] is true, it displays a loading spinner instead.
+class Button extends StatefulWidget {
   Button({
     @required this.child,
-    this.isRaised = true,
     @required this.onPressed,
-    this.onSuccess,
-    this.onError,
+    this.isLoading = false,
   })  : assert(child != null),
-        assert(isRaised != null),
-        assert(onPressed != null);
+        assert(onPressed != null),
+        assert(isLoading != null);
 
   final Widget child;
-  final bool isRaised;
-  final FutureOr<T> Function() onPressed;
-  final dynamic Function(T result) onSuccess;
-  final void Function(dynamic error) onError;
+  final VoidCallback onPressed;
+  final bool isLoading;
 
-  _ButtonState createState() => _ButtonState<T>();
+  _ButtonState createState() => _ButtonState();
 }
 
 class _ButtonState<T> extends State<Button> {
-  var _isLoading = false;
-
-  Future<void> _onPressed() async {
-    try {
-      setState(() => _isLoading = true);
-      T result = await widget.onPressed();
-      if (widget.onSuccess != null) widget.onSuccess(result);
-    } catch (e) {
-      setState(() => _isLoading = false);
-      if (widget.onError != null) widget.onError(e);
-    }
-  }
+  bool get _isLoading => widget.isLoading;
 
   @override
   Widget build(BuildContext context) {
@@ -51,16 +30,12 @@ class _ButtonState<T> extends State<Button> {
 
     return RawMaterialButton(
       // Do not handle touch events if the button is already loading.
-      onPressed: _isLoading ? () {} : _onPressed,
-      fillColor: widget.isRaised ? theme.primaryColor : null,
+      onPressed: _isLoading ? () {} : widget.onPressed,
+      fillColor: theme.primaryColor,
       highlightColor: Colors.black.withOpacity(0.08),
-      splashColor: _isLoading
-          ? Colors.transparent
-          : widget.isRaised
-              ? Colors.black26
-              : theme.primaryColor.withOpacity(0.3),
-      elevation: widget.isRaised ? 2 : 0,
-      highlightElevation: widget.isRaised ? 2 : 0,
+      splashColor: _isLoading ? Colors.transparent : Colors.black26,
+      elevation: 2,
+      highlightElevation: 2,
       shape: _isLoading
           ? const CircleBorder()
           : RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
--- lib/login/widgets/login_screen.dart
+++ lib/login/widgets/login_screen.dart
@@ -28,27 +28,48 @@ class _LoginContentState extends State<LoginContent>
     with BlocConsumer<Bloc, LoginContent> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _alreadyTriedToSignIn = false;
+  bool _isLoading = false;
+  bool _isEmailValid = true;
+  bool _isPasswordValid = true;
+  String _ambientError;
 
-  Future<void> _executeLogin(Future<void> Function() login) async {
-    try {
-      await login();
-    } on NoConnectionToServerError catch (_) {
-      // TODO: display error to the user
-    } on AuthenticationError catch (e) {
-      print(e);
-      // TODO: display error to the user
-    }
-
-    // Logged in.
-    Navigator.of(context).pushReplacement(MaterialPageRoute(
-      builder: (context) => DashboardScreen(),
-    ));
+  void _checkSyntax() {
+    setState(() {
+      _isEmailValid =
+          !_alreadyTriedToSignIn || bloc.isEmailValid(_emailController.text);
+      _isPasswordValid = !_alreadyTriedToSignIn ||
+          bloc.isPasswordValid(_passwordController.text);
+    });
   }
 
-  Future<void> _login() => _executeLogin(() => bloc.login(
-        _emailController.text,
-        _passwordController.text,
+  Future<void> _executeLogin(Future<void> Function() login) async {
+    _alreadyTriedToSignIn = true;
+    setState(() => _isLoading = true);
+
+    try {
+      await login();
+      setState(() => _ambientError = null);
+
+      // Logged in.
+      await Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => DashboardScreen(),
       ));
+    } on NoConnectionToServerError catch (_) {
+      setState(() => _ambientError = "No connection to the server.");
+    } on AuthenticationError catch (_) {
+      setState(() => _ambientError = "Authentication failed.");
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  Future<void> _login() async {
+    await _executeLogin(() async {
+      _checkSyntax();
+      await bloc.login(_emailController.text, _passwordController.text);
+    });
+  }
 
   Future<void> _loginAsDemoStudent() =>
       _executeLogin(() => bloc.loginAsDemoStudent());
@@ -76,14 +97,36 @@ class _LoginContentState extends State<LoginContent>
 
   List<Widget> _buildContent() {
     return [
-      _buildLogo(),
+      FlutterLogo(size: 100, colors: Colors.red),
       SizedBox(height: 16),
-      _buildEmailField(),
+      LoginInput(
+        controller: _emailController,
+        label: 'Email',
+        error: _isEmailValid ? null : 'Enter an email address.',
+        onChanged: _checkSyntax,
+      ),
       SizedBox(height: 16),
-      _buildPasswordField(),
+      LoginInput(
+        controller: _passwordController,
+        label: 'Password',
+        obscureText: true,
+        error: _isPasswordValid ? null : 'Enter a password.',
+        onChanged: _checkSyntax,
+      ),
       SizedBox(height: 16),
-      _buildLoginButton(),
+      Button(
+        onPressed: _login,
+        isLoading: _isLoading,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Text(
+            _isLoading ? 'Loading' : 'Login',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
       SizedBox(height: 8),
+      if (_ambientError != null) Text(_ambientError),
       Divider(),
       SizedBox(height: 8),
       Text("Don't have an account yet? Try it out!"),
@@ -94,56 +137,33 @@ class _LoginContentState extends State<LoginContent>
       ),
     ];
   }
-
-  Widget _buildLogo() {
-    return FlutterLogo(size: 100, colors: Colors.red);
-  }
-
-  Widget _buildEmailField() {
-    return LoginInput(controller: _emailController, label: 'Email');
-  }
-
-  Widget _buildPasswordField() {
-    return LoginInput(
-      controller: _passwordController,
-      label: 'Password',
-      obscureText: true,
-    );
-  }
-
-  Widget _buildLoginButton() {
-    return Button<void>(
-      onPressed: _login,
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Text(
-          'Login',
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-      ),
-    );
-  }
 }
 
 class LoginInput extends StatelessWidget {
   LoginInput({
     @required this.controller,
     @required this.label,
+    this.error,
     this.obscureText = false,
+    this.onChanged,
   });
 
   final TextEditingController controller;
   final String label;
+  final String error;
   final bool obscureText;
+  final VoidCallback onChanged;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
       obscureText: obscureText,
+      onChanged: (_) => onChanged(),
       decoration: InputDecoration(
         border: OutlineInputBorder(),
         labelText: label,
+        errorText: error,
       ),
     );
   }
