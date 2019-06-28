4bc9988de7489996eb92c378dd45c76e5b7d351e
Marcel Garus <marcel.garus@gmail.com>
2019-06-28 21:35:29 +0200

    refactor(core): make AuthenticationStorage synchronous (WIP)

--- lib/core/services/authentication_storage.dart
+++ lib/core/services/authentication_storage.dart
@@ -1,21 +1,36 @@
+import 'package:flutter/foundation.dart';
+
 import 'package:schulcloud/core/data.dart';
+
+/*@immutable
+class User {
+  final String email;
+  final String token;
+
+  User({
+    @required this.email,
+    @required this.token,
+  });
+}*/
 
 /// A service that offers storage of an email and an access token. It doesn't do
 /// the actual login.
 class AuthenticationStorageService {
-  Repository<String> _repo = SharedPreferences(
-    keyPrefix: 'authentication',
+  static final _inMemory = InMemoryStorage<String>();
+  static final _repo = CachedRepository<String>(
+    cache: _inMemory,
+    source: SharedPreferences(keyPrefix: 'authentication'),
   );
-  static const _emailKey = Id<String>('email');
-  static const _tokenKey = Id<String>('token');
 
-  Stream<String> fetchEmail() => _repo.fetch(_emailKey);
-  Stream<String> fetchToken() => _repo.fetch(_tokenKey);
-  Future<bool> checkAuthorization() async =>
-      (await _repo.fetchAllIds().first).contains(_tokenKey);
+  static const _emailId = Id<String>('email');
+  static const _tokenId = Id<String>('token');
+
+  String get email => _inMemory[_emailId];
+  String get token => _inMemory[_tokenId];
+  bool get isAuthorized => token != null;
+
+  set email(String email) => _repo.update(_emailId, email);
+  set token(String token) => _repo.update(_tokenId, token);
 
   void logout() => _repo.clear();
-
-  set email(String email) => _repo.update(_emailKey, email);
-  set token(String token) => _repo.update(_tokenKey, token);
 }
