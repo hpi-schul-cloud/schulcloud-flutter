173644ef38674a2998242499b41a1d8bfcd5dce5
Marcel Garus <marcel.garus@gmail.com>
2019-06-28 21:34:58 +0200

    feat(core): add listNews to api (WIP)

--- lib/core/services/api.dart
+++ lib/core/services/api.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 
+import 'package:schulcloud/core/data.dart';
+import 'package:schulcloud/news/entities.dart';
+
 import 'network.dart';
 
 class AuthenticationError {}
@@ -23,5 +26,40 @@ class ApiService {
 
     return (json.decode(response.body) as Map<String, dynamic>)['accessToken']
         as String;
+  }
+
+  Future<List<Article>> listNews() async {
+    var response = await network.get('news?');
+
+    switch (response.statusCode) {
+      case 401:
+        throw AuthenticationError();
+      case 200:
+        break;
+      default:
+        throw Error(); // Something bad happened.
+    }
+
+    var body = json.decode(response.body);
+    return (body['data'] as List<dynamic>).map((data) {
+      data = data as Map<String, dynamic>;
+      return Article(
+        id: Id<Article>(data['_id']),
+        title: data['title'],
+        author: Author(
+          id: Id<Author>(data['creator']['_id']),
+          name:
+              '${data['creator']['firstName']} ${data['creator']['lastName']}',
+        ),
+        section: 'Section',
+        published: DateTime.parse(data['displayAt']),
+        content: data['content'],
+      );
+    }).toList();
+  }
+
+  Future<Article> getArticle(Id<Article> id) async {
+    var response = await network.get('news/{id}');
+    // TODO: parse article
   }
 }
