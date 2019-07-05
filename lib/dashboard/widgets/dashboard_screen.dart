59108914f53e5b75c8c92ab609b79b2a802d1503
Marcel Garus <marcel.garus@gmail.com>
2019-07-05 22:50:07 +0200

    refactor(dashboard): use built-in bottom bar

--- lib/dashboard/widgets/dashboard_screen.dart
+++ lib/dashboard/widgets/dashboard_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:schulcloud/news/news.dart';
-
-import 'menu.dart';
+import 'package:schulcloud/core/widgets.dart';
 
 class DashboardScreen extends StatefulWidget {
   @override
@@ -10,36 +8,11 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  void _showMenu() {
-    showModalBottomSheet(context: context, builder: (context) => Menu());
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlutterLogo()
-      ),
-      bottomNavigationBar: _buildBottomAppBar(),
-    );
-  }
-
-  Widget _buildBottomAppBar() {
-    return BottomAppBar(
-      color: Theme.of(context).primaryColor,
-      elevation: 6,
-      child: Container(
-        height: 56,
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Row(
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.menu, color: Colors.white),
-              onPressed: _showMenu,
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: FlutterLogo()),
+      bottomNavigationBar: MyAppBar(),
     );
   }
 }
