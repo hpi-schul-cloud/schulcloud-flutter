7a74fa0bdba250eb65f624e0e65cb1fef24b4e28
Marcel Garus <marcel.garus@gmail.com>
2019-07-05 22:48:43 +0200

    feat(core): add app bar

---
+++ lib/core/widgets/app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'menu.dart';
+
+class MyAppBar extends StatefulWidget {
+  @override
+  _MyAppBarState createState() => _MyAppBarState();
+}
+
+class _MyAppBarState extends State<MyAppBar> {
+  Future<void> _showMenu() async {
+    String targetScreen = await showModalBottomSheet(
+      context: context,
+      builder: (context) => Menu(),
+    );
+
+    if (targetScreen != null)
+      Navigator.pushReplacementNamed(context, targetScreen);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: 'bottom_app_bar',
+      child: BottomAppBar(
+        color: Theme.of(context).primaryColor,
+        elevation: 6,
+        child: Container(
+          height: 56,
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.menu, color: Colors.white),
+                onPressed: _showMenu,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
