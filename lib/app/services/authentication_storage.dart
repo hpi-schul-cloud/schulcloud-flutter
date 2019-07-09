fad7bbbfe5e070afa040f5c396f66648cb99b64f
Marcel Garus <marcel.garus@gmail.com>
2019-07-09 22:24:58 +0200

    feat: add support for onLoaded listeners

--- lib/app/services/authentication_storage.dart
+++ lib/app/services/authentication_storage.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:schulcloud/core/data.dart';
 
 /// A service that offers storage of an email and an access token. It doesn't do
@@ -9,16 +11,29 @@ class AuthenticationStorageService {
   final _inMemory = InMemoryStorage<String>();
   CachedRepository<String> _repo;
 
+  bool _isLoaded = false;
+  final _onLoadedListeners = <VoidCallback>{};
+
   AuthenticationStorageService() {
     _repo = CachedRepository<String>(
       cache: _inMemory,
       source: SharedPreferences('authentication'),
-    )..loadItemsIntoCache();
+    )..loadItemsIntoCache().then((_) {
+        _isLoaded = true;
+        _onLoadedListeners.forEach((listener) => listener());
+      });
+  }
+
+  void addOnLoadedListener(VoidCallback listener) {
+    if (_isLoaded)
+      listener();
+    else
+      this._onLoadedListeners.add(listener);
   }
 
   String get email => _inMemory.get(_emailId);
   String get token => _inMemory.get(_tokenId);
-  bool get isAuthorized => token != null;
+  bool get isAuthenticated => token != null;
 
   set email(String email) => _repo.update(_emailId, email);
   set token(String token) => _repo.update(_tokenId, token);
