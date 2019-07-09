bb9294e5a448611cd5e7bcd92d2d46487837ff9d
Marcel Garus <marcel.garus@gmail.com>
2019-07-09 22:30:47 +0200

    feat: add splash screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 abstract class Routes {
+  static const String splashScreen = 'splashScreen';
   static const String login = 'login';
   static const String dashboard = 'dashboard';
   static const String news = 'news';
