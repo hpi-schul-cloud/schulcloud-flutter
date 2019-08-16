93b7c9de004ea296ab79d0b53253fb4527f57698
Marcel Garus <marcel.garus@gmail.com>
2019-08-16 21:52:44 +0200

    feat(core): add hive repository

--- lib/core/data/repositories.dart
+++ lib/core/data/repositories.dart
@@ -1,5 +1,6 @@
 export 'repositories/base_dao.dart';
 export 'repositories/cached_repository.dart';
+export 'repositories/hive.dart';
 export 'repositories/in_memory_storage.dart';
 export 'repositories/json_to_string_transformer.dart';
 export 'repositories/object_to_json_transformer.dart';
