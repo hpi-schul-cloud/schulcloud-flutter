e969761500abc4ffdf5b6028dd6ce0bc05d3881a
Marcel Garus <marcel.garus@gmail.com>
2019-08-22 19:25:00 +0200

    refactor: remove unused PaginatedLoader

--- lib/app/widgets.dart
+++ lib/app/widgets.dart
@@ -1,6 +1,5 @@
 export 'widgets/app_bar.dart';
 export 'widgets/bloc_consumer.dart';
 export 'widgets/menu.dart';
-export 'widgets/paginated_loader.dart';
 export 'widgets/placeholder.dart';
 export 'widgets/staggered_column.dart';
