43bfd3927e6ff1be1fe673d3854c9f1f0b89e5fd
Marcel Garus <marcel.garus@gmail.com>
2019-09-07 12:36:56 +0200

    refactor: remove build.yaml

--- .dart_tool/build/entrypoint/build.dart
+++ .dart_tool/build/entrypoint/build.dart
@@ -1,25 +1,29 @@
 // ignore_for_file: directives_ordering
 
 import 'package:build_runner_core/build_runner_core.dart' as _i1;
-import 'package:json_serializable/builder.dart' as _i2;
-import 'package:source_gen/builder.dart' as _i3;
-import 'package:build_config/build_config.dart' as _i4;
-import 'dart:isolate' as _i5;
-import 'package:build_runner/build_runner.dart' as _i6;
-import 'dart:io' as _i7;
+import 'package:hive_generator/hive_generator.dart' as _i2;
+import 'package:json_serializable/builder.dart' as _i3;
+import 'package:source_gen/builder.dart' as _i4;
+import 'package:build_config/build_config.dart' as _i5;
+import 'dart:isolate' as _i6;
+import 'package:build_runner/build_runner.dart' as _i7;
+import 'dart:io' as _i8;
 
 final _builders = <_i1.BuilderApplication>[
-  _i1.apply('json_serializable:json_serializable', [_i2.jsonSerializable],
+  _i1.apply('hive_generator:hive_generator', [_i2.getBuilder],
+      _i1.toDependentsOf('hive_generator'),
+      hideOutput: true, appliesBuilders: ['source_gen:combining_builder']),
+  _i1.apply('json_serializable:json_serializable', [_i3.jsonSerializable],
       _i1.toDependentsOf('json_serializable'),
       hideOutput: true, appliesBuilders: ['source_gen:combining_builder']),
-  _i1.apply('source_gen:combining_builder', [_i3.combiningBuilder],
+  _i1.apply('source_gen:combining_builder', [_i4.combiningBuilder],
       _i1.toNoneByDefault(),
       hideOutput: false, appliesBuilders: ['source_gen:part_cleanup']),
-  _i1.applyPostProcess('source_gen:part_cleanup', _i3.partCleanup,
-      defaultGenerateFor: const _i4.InputSet())
+  _i1.applyPostProcess('source_gen:part_cleanup', _i4.partCleanup,
+      defaultGenerateFor: const _i5.InputSet())
 ];
-main(List<String> args, [_i5.SendPort sendPort]) async {
-  var result = await _i6.run(args, _builders);
+main(List<String> args, [_i6.SendPort sendPort]) async {
+  var result = await _i7.run(args, _builders);
   sendPort?.send(result);
-  _i7.exitCode = result;
+  _i8.exitCode = result;
 }
