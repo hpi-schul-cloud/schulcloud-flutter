826833af74c336506df0d710ea5e665ea03c39f4
Marcel Garus <marcel.garus@gmail.com>
2019-09-20 14:07:19 +0200

    refactor: remove unused import

--- lib/homework/widgets/homework_detail_screen.dart
+++ lib/homework/widgets/homework_detail_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:provider/provider.dart';
 import 'package:schulcloud/app/app.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../bloc.dart';
 import '../data.dart';
