732d2c4fb9362bfb491c27180c942e8b8dcfb7e3
Clemens Tiedt <clemenstiedt@gmx.de>
2019-10-12 17:21:18 +0200

    feat(dashboard): add DashboardCard widget and homework card

--- lib/dashboard/widgets/dashboard_screen.dart
+++ lib/dashboard/widgets/dashboard_screen.dart
@@ -1,41 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:schulcloud/dashboard/widgets/dashboard_card.dart';
+import 'package:schulcloud/homework/widgets/dashboard_card.dart';
 
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: FlutterLogo(),
-    ));
+      body: ListView(
+        children: <Widget>[
+          SizedBox(height: 16),
+          DashboardCard(
+            title: 'Stundenplan',
+            child: FlutterLogo(),
+          ),
+          HomeworkDashboardCard(),
+          DashboardCard(
+            title: 'News',
+            child: FlutterLogo(),
+          ),
+        ],
+      ),
+    );
   }
-}
-
-Card createDashboardItem(String title, IconData icon, Color color) {
-  return Card(
-      elevation: 1.0,
-      margin: new EdgeInsets.all(8.0),
-      child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0), color: Colors.white),
-          child: new InkWell(
-              onTap: () {},
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                verticalDirection: VerticalDirection.down,
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  Center(
-                      child: Icon(
-                    icon,
-                    size: 50.0,
-                    color: color,
-                  )),
-                  SizedBox(height: 20.0),
-                  new Center(
-                    child: new Text(title,
-                        style: new TextStyle(fontSize: 18.0, color: color)),
-                  )
-                ],
-              ))));
 }
