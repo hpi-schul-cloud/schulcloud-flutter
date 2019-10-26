5140ea43df9dbd7a416bc11663525f186903e5d9
Marcel Garus <marcel.garus@gmail.com>
2019-10-27 01:28:47 +0200

    feat: add ErrorBanner widget

---
+++ lib/app/widgets/error_banner.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../services/network.dart';
+import '../utils.dart';
+import 'buttons.dart';
+import 'empty_state.dart';
+
+/// A screen that displays an [error].
+class ErrorBanner extends StatelessWidget {
+  final dynamic error;
+  final VoidCallback onRetry;
+
+  ErrorBanner(this.error, {this.onRetry}) : assert(error != null);
+
+  @override
+  Widget build(BuildContext context) {
+    String message;
+    List<Widget> actions = [];
+
+    if (error is NoConnectionToServerError) {
+      message = "We can't connect to the server.\n"
+          "Are you sure you're connected to the internet?";
+    } else if (error is AuthenticationError) {
+      message = "Seems like this device's authentication expired.\n"
+          "Maybe logging out and in again helps?";
+      actions.add(SecondaryButton(
+        child: Text('Log out'),
+        onPressed: () => logOut(context),
+      ));
+    } else {
+      message = "Oh no! An internal error occurred:\n$error";
+    }
+
+    return Material(
+      elevation: 4,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text(message),
+        ),
+      ),
+    );
+  }
+}
