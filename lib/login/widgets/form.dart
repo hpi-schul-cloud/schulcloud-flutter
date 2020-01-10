9fa507384b7960b454ca8ddc04a8c1c4ea0aa03c
Jonas Wanke <contact@jonas-wanke.com>
2020-01-10 12:56:09 +0100

    fix(login): improve logo size

--- lib/login/widgets/form.dart
+++ lib/login/widgets/form.dart
@@ -79,8 +79,14 @@ class _LoginFormState extends State<LoginForm> {
       width: 400,
       child: Column(
         children: [
-          SvgPicture.asset(
-            AppConfig.of(context).assetName('logo/logo_with_text.svg'),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            child: SvgPicture.asset(
+              AppConfig.of(context)
+                  .assetName(context, 'logo/logo_with_text.svg'),
+              height: 64,
+              alignment: Alignment.bottomCenter,
+            ),
           ),
           SizedBox(height: 16),
           LoginInput(
