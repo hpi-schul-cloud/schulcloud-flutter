75a48fb6f21a17418c3b56e0c62807f85568d452
Jonas Wanke <contact@jonas-wanke.com>
2020-01-21 17:05:11 +0100

    refactor: remove generated l10n files from git

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'package:schulcloud/app/app.dart';
 import 'package:time_machine/time_machine.dart';
