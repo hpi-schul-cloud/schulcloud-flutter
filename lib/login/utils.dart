26b96b1085976c1efb116e7bb80f7a533d44b090
Marcel Garus <marcel.garus@gmail.com>
2020-01-22 13:56:52 +0100

    feat: show logout confirmation dialog

--- lib/login/utils.dart
+++ lib/login/utils.dart
@@ -1,16 +1,43 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:pedantic/pedantic.dart';
 import 'package:provider/provider.dart';
 import 'package:schulcloud/app/app.dart';
+import 'package:schulcloud/generated/generated.dart';
 
 import 'login.dart';
 
-Future<void> logOut(BuildContext context) async {
-  await Provider.of<StorageService>(context).clear();
+Future<bool> logOut(BuildContext context) async {
+  final confirmed = await showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(context.s.app_logOut_title),
+        content: Text(context.s.app_logOut_content),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text(context.s.general_cancel),
+          ),
+          FlatButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: Text(context.s.app_logOut_confirm),
+          ),
+        ],
+      );
+    },
+  );
 
-  final navigator = Navigator.of(context, rootNavigator: true)
-    ..popUntil((route) => route.isFirst);
-  unawaited(navigator.pushReplacement(TopLevelPageRoute(
-    builder: (_) => LoginScreen(),
-  )));
+  if (confirmed) {
+    // Actually log out.
+
+    unawaited(Provider.of<StorageService>(context, listen: false).clear());
+
+    final navigator = Navigator.of(context, rootNavigator: true)
+      ..popUntil((route) => route.isFirst);
+    unawaited(navigator.pushReplacement(TopLevelPageRoute(
+      builder: (_) => LoginScreen(),
+    )));
+  }
+
+  return confirmed;
 }
