a9a140cdb94b3390ab865d7c8ec3bac3d1ea303f
Jonas Wanke <contact@jonas-wanke.com>
2020-03-01 14:21:26 +0100

    ui(app): improve account dialog design

--- lib/app/widgets/account_dialog.dart
+++ lib/app/widgets/account_dialog.dart
@@ -1,39 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cached/flutter_cached.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:schulcloud/app/app.dart';
 import 'package:schulcloud/login/login.dart';
 import 'package:schulcloud/settings/settings.dart';
 
 import '../data.dart';
 import '../services/user_fetcher.dart';
+import 'account_avatar.dart';
 
 class AccountDialog extends StatelessWidget {
-  void _openSettings(BuildContext context) {
-    context.navigator.push(MaterialPageRoute(
-      builder: (_) => SettingsScreen(),
-    ));
-  }
-
   @override
   Widget build(BuildContext context) {
-    AboutDialog();
-    return AlertDialog(
-      content: CachedRawBuilder<User>(
-        controller: services.get<UserFetcherService>().fetchCurrentUser(),
-        builder: (context, update) {
-          return Text(update?.data?.displayName ?? context.s.general_loading);
-        },
+    return Dialog(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              child: SvgPicture.asset(
+                services
+                    .get<AppConfig>()
+                    .assetName(context, 'logo/logo_with_text.svg'),
+                height: 64,
+                alignment: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          CachedRawBuilder<User>(
+            controller: services.get<UserFetcherService>().fetchCurrentUser(),
+            builder: (context, update) {
+              final user = update.data;
+              return ListTile(
+                leading: AccountAvatar(),
+                title: Text(user?.name ?? context.s.general_loading),
+                subtitle: Text(user?.email ?? ''),
+              );
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () {
+              context.navigator.pushReplacement(MaterialPageRoute(
+                builder: (_) => SettingsScreen(),
+              ));
+            },
+          ),
+          ListTile(
+            leading: SvgPicture.asset(
+              'assets/icon_logout.svg',
+              color: Colors.black45,
+              width: 24,
+            ),
+            title: Text('Log out'),
+            onTap: () => logOut(context),
+          ),
+        ],
       ),
-      actions: <Widget>[
-        FlatButton(
-          onPressed: () => _openSettings(context),
-          child: Text('Settings'),
-        ),
-        FlatButton(
-          onPressed: () => logOut(context),
-          child: Text('Log out'),
-        ),
-      ],
     );
   }
 }
