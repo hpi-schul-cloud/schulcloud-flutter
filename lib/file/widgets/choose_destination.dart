bb4a7b9b94311ae9e5580cc32e9972f11c9c18c1
Marcel Garus <marcel.garus@gmail.com>
2020-03-12 17:59:50 +0100

    add ChooseDestinationScreen

---
+++ lib/file/widgets/choose_destination.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:schulcloud/app/app.dart';
+
+import '../data.dart';
+
+@immutable
+class FileDestination {
+  const FileDestination._({@required this.ownerId, this.parentId});
+
+  final Id<dynamic> ownerId;
+  final Id<File> parentId;
+}
+
+class ChooseDestinationScreen extends StatelessWidget {
+  const ChooseDestinationScreen({
+    @required this.title,
+    @required this.buttonContent,
+  })  : assert(title != null),
+        assert(buttonContent != null);
+
+  final Widget title;
+  final Widget buttonContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () => context.navigator.pop(),
+        ),
+        title: title,
+      ),
+      body: Center(
+        child: Text("For now, you can't choose a destination.\n"
+            'The destination will be the root of your personal files.'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        onPressed: () => context.navigator.pop(FileDestination._(
+          ownerId: services.storage.userId,
+          parentId: null,
+        )),
+        child: IconTheme(
+          data: IconThemeData(color: context.theme.primaryColor),
+          child: buttonContent,
+        ),
+      ),
+    );
+  }
+}
