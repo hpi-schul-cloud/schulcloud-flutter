9c673213ebfd9ee6318982a1b32e650943d9661a
Marcel Garus <marcel.garus@gmail.com>
2020-03-12 22:25:29 +0100

    add FancyCachedBuilder

---
+++ lib/app/widgets/cached_builder.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cached/flutter_cached.dart';
+import 'package:schulcloud/app/app.dart';
+
+class FancyCachedBuilder<T> extends StatelessWidget {
+  const FancyCachedBuilder({@required this.controller, @required this.builder})
+      : assert(controller != null),
+        assert(builder != null);
+
+  final CacheController<T> controller;
+  final Widget Function(BuildContext, T data, bool isFetching) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedRawBuilder(
+      controller: controller,
+      builder: (context, update) {
+        // There is no error. We can just call the builder.
+        if (update.hasNoError) {
+          return builder(context, update.data, update.isFetching);
+        }
+
+        // An error occurred.
+
+        // Our code should only throw [FancyException]s to trigger behavior in
+        // other parts of the app. Other exceptions should be handled by the
+        // children of this widget. If they can't handle them, they should
+        // catch them nevertheless and then throw [FancyException]s instead.
+        if (update.error is! FancyException) {
+          logger.e(update.error);
+          return StripedErrorWidget(update.error, update.stackTrace);
+        }
+
+        final error = update.error as FancyException;
+
+        // If there are no data, then there's nothing we can display except the
+        // error.
+        if (update.hasNoData) {
+          return ErrorScreen(update.error, update.stackTrace);
+        }
+
+        // There is an error as well as cached data.
+
+        // If the error is global, it's been handled elsewhere. More
+        // specifically, the user is shown a message at a central error space.
+        // That's why we don't need to display the error here.
+        if (error.isGlobal) {
+          return builder(context, update.data, update.isFetching);
+        }
+
+        // TODO(marcelgarus): Display both the error and the cached content.
+        return StripedErrorWidget(update.error, update.stackTrace);
+      },
+    );
+  }
+}
