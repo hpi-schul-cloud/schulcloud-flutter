7397b46dc6e0ba8c7340992e72d216fb2c071335
Marcel Garus <marcel.garus@gmail.com>
2020-03-12 22:23:58 +0100

    add FancyException

---
+++ lib/app/exception.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+typedef ErrorMessageBuilder = String Function(BuildContext context);
+
+/// All the exceptions that are thrown in this repository and are expected to
+/// be handled by other code in this repository should extend [FancyException].
+class FancyException implements Exception {
+  FancyException({
+    @required this.isGlobal,
+    @required this.messageBuilder,
+  })  : assert(isGlobal != null),
+        assert(messageBuilder != null);
+
+  /// Whether the error applies to the whole app. Certain errors indicate that
+  /// the whole app is in a certain exceptional state, for example when there's
+  /// no connection to the server or the user's authentication token expired.
+  final bool isGlobal;
+
+  /// Creates a localized message describing this error.
+  final ErrorMessageBuilder messageBuilder;
+
+  @Deprecated('To display FancyExceptions, use the messageBuilder instead of '
+      'toString(). It takes a context and returns a localized error message.')
+  @override
+  String toString() => '$runtimeType';
+}
