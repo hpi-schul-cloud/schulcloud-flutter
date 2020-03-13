ee2cf30bf9f0f654dda0f1d70c623d561b73a75e
Marcel Garus <marcel.garus@gmail.com>
2020-03-13 17:54:27 +0100

    add FancyCachedBuilder.handleLoading constructor

--- lib/app/widgets/cached_builder.dart
+++ lib/app/widgets/cached_builder.dart
@@ -2,17 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cached/flutter_cached.dart';
 import 'package:schulcloud/app/app.dart';
 
+typedef CachedBuilderContentBuilder<T> = Widget Function(
+    BuildContext, T data, bool isFetching);
+
 class FancyCachedBuilder<T> extends StatelessWidget {
   const FancyCachedBuilder({@required this.controller, @required this.builder})
       : assert(controller != null),
         assert(builder != null);
 
+  factory FancyCachedBuilder.handleLoading({
+    CacheController<T> controller,
+    CachedBuilderContentBuilder<T> builder,
+  }) =>
+      FancyCachedBuilderWithLoading(
+        controller: controller,
+        builder: builder,
+      );
+
   final CacheController<T> controller;
-  final Widget Function(BuildContext, T data, bool isFetching) builder;
+  final CachedBuilderContentBuilder<T> builder;
 
   @override
   Widget build(BuildContext context) {
-    return CachedRawBuilder(
+    return CachedRawBuilder<T>(
       controller: controller,
       builder: (context, update) {
         // There is no error. We can just call the builder.
@@ -21,20 +33,23 @@ class FancyCachedBuilder<T> extends StatelessWidget {
         }
 
         // An error occurred.
+        assert(update.hasError);
 
         // Our code should only throw [FancyException]s to trigger behavior in
         // other parts of the app. Other exceptions should be handled by the
         // children of this widget. If they can't handle them, they should
         // catch them nevertheless and then throw [FancyException]s instead.
         if (update.error is! FancyException) {
-          logger
-            ..e('The following exception occurred. If you want to display an '
-                'exception to the user, instead create a custom exception by '
-                'extending FancyException. This will be caught and display a '
-                'beautiful error screen.')
-            ..e(update.error)
-            ..e('To view the full stack trace, long-tap the pink striped '
-                'area.');
+          logger.e(
+            'The following exception occurred: '
+            '${update.error} '
+            'If you want to display an exception to the user, instead create '
+            'a custom exception by extending FancyException. This will be '
+            'caught and display a beautiful error screen. '
+            'To view the full stack trace, long-tap the pink striped area.',
+            update.error,
+            update.stackTrace,
+          );
           return PinkStripedErrorWidget(update.error, update.stackTrace);
         }
 
@@ -43,7 +58,7 @@ class FancyCachedBuilder<T> extends StatelessWidget {
         // If there are no data, then there's nothing we can display except the
         // error.
         if (update.hasNoData) {
-          return ErrorScreen(update.error, update.stackTrace);
+          return ErrorScreen(update.error);
         }
 
         // There is an error as well as cached data.
@@ -60,4 +75,18 @@ class FancyCachedBuilder<T> extends StatelessWidget {
       },
     );
   }
+}
+
+class FancyCachedBuilderWithLoading<T> extends FancyCachedBuilder<T> {
+  FancyCachedBuilderWithLoading({
+    @required CacheController<T> controller,
+    @required CachedBuilderContentBuilder<T> builder,
+  }) : super(
+          controller: controller,
+          builder: (context, data, isFetching) {
+            return data == null
+                ? Center(child: CircularProgressIndicator())
+                : builder(context, data, isFetching);
+          },
+        );
 }
