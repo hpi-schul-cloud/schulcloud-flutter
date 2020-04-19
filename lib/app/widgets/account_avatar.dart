042979b99f3c0e188a9716b279ce648a9bf607ee
Marcel Garus <marcel.garus@gmail.com>
2020-04-19 13:37:30 +0200

    rearrange code to be top-down

--- lib/app/widgets/account_avatar.dart
+++ lib/app/widgets/account_avatar.dart
@@ -9,6 +9,46 @@ import '../utils.dart';
 import 'account_dialog.dart';
 import 'cache_utils.dart';
 
+class AccountButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      customBorder: CircleBorder(),
+      onTap: () {
+        showDialog(context: context, builder: (_) => AccountDialog());
+      },
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: ValueListenableBuilder<Set<Banner>>(
+          valueListenable: services.banners,
+          builder: (context, banners, _) {
+            return banners.contains(Banners.demo)
+                ? Center(child: DemoTag())
+                : AccountAvatar();
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class DemoTag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Color.lerp(Colors.white, Colors.orange, 0.5),
+      borderRadius: BorderRadius.circular(8),
+      child: Padding(
+        padding: EdgeInsets.all(4),
+        child: Text(
+          'DEMO', // TODO: localize
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
 class AccountAvatar extends StatelessWidget {
   const AccountAvatar();
 
@@ -38,46 +78,6 @@ class AccountAvatar extends StatelessWidget {
           ),
         );
       }),
-    );
-  }
-}
-
-class DemoTag extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Color.lerp(Colors.white, Colors.orange, 0.5),
-      borderRadius: BorderRadius.circular(8),
-      child: Padding(
-        padding: EdgeInsets.all(4),
-        child: Text(
-          'DEMO', // TODO: localize
-          style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
-}
-
-class AccountButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      customBorder: CircleBorder(),
-      onTap: () {
-        showDialog(context: context, builder: (_) => AccountDialog());
-      },
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ValueListenableBuilder<Set<Banner>>(
-          valueListenable: services.banners,
-          builder: (context, banners, _) {
-            return banners.contains(Banners.demo)
-                ? Center(child: DemoTag())
-                : AccountAvatar();
-          },
-        ),
-      ),
     );
   }
 }
