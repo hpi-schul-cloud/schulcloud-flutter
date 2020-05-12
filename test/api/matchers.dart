4d3f2389eb82d22fb0dea8a8fea904ac4592db9d
Jonas Wanke <contact@jonas-wanke.com>
2020-05-12 08:38:48 +0200

    test(assignment): add GET /homework/:id test

--- test/api/matchers.dart
+++ test/api/matchers.dart
@@ -1,8 +1,10 @@
 import 'package:test/test.dart';
 
 final isColorString = matches(RegExp('#[0-9a-f]{6}', caseSensitive: false));
-final isId = isA<String>();
+
+final isId = allOf(isA<String>(), isNotEmpty);
 final isIdList = allOf(isList, everyElement(isId));
+final isEmptyIdList = allOf(isIdList, isEmpty);
 
 Matcher matchesJsonMap(Map<String, dynamic> map) =>
     allOf(map.entries.map((e) => containsPair(e.key, e.value)).toList());
