eb572b4274376aca8b256e3b999f1c2d4d63e295
Jonas Wanke <contact@jonas-wanke.com>
2020-07-03 12:30:36 +0200

    feat(messenger): display room list

--- lib/messenger/pages/messenger_page.dart
+++ lib/messenger/pages/messenger_page.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/widgets.dart';
+import 'package:matrix_sdk/matrix_sdk.dart';
 import 'package:schulcloud/app/app.dart';
+
+import '../service.dart';
+import '../widgets/room_list_tile.dart';
 
 class MessengerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FancyScaffold(
       appBar: FancyAppBar(title: Text(context.s.messenger)),
-      sliver: SliverList(
-        delegate: SliverChildListDelegate([]),
+      omitHorizontalPadding: true,
+      sliver: StreamBuilder<List<Room>>(
+        stream: services.messenger.stream.map((u) => u.rooms.toList()),
+        builder: fetchableToAsync(handleLoadingErrorEmptySliver(
+          emptyStateBuilder: (_) =>
+              EmptyStateScreen(text: "You aren't part of any room yet."),
+          builder: (context, rooms, _) {
+            return SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => RoomListTile(rooms[index]),
+                childCount: rooms.length,
+              ),
+            );
+          },
+        )),
       ),
     );
+  }
+
+  AsyncWidgetBuilder<T> fetchableToAsync<T>(
+    FetchableBuilder<CacheSnapshot<T>> builder,
+  ) {
+    return (context, snapshot) {
+      final cacheSnapshot = CacheSnapshot(
+        data: snapshot.data,
+        hasData: snapshot.hasData,
+        error: snapshot.error,
+      );
+      return builder(
+        context,
+        cacheSnapshot,
+        ({force}) => services.messenger.sync(),
+      );
+    };
   }
 }
