1e19ddb843250f8ed797cc4d8070bdd335354a7a
Marcel Garus <marcel.garus@gmail.com>
2020-09-02 14:08:19 +0200

    Abort if a command fails

--- build-apk.sh
+++ build-apk.sh
@@ -1,4 +1,1 @@
-flutter pub get
-flutter pub run build_runner build
-flutter pub run intl_utils:generate
-flutter build apk --flavor=sc
+flutter pub get && flutter pub run build_runner build && flutter pub run intl_utils:generate && flutter build apk --flavor=sc
