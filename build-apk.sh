78e359497c18ee509eaacc304850e2b2f8a8166f
Marcel Garus <marcel.garus@gmail.com>
2020-09-02 16:58:05 +0200

    Ensure build_runner deletes conflicting outputs

--- build-apk.sh
+++ build-apk.sh
@@ -1,4 +1,4 @@
 flutter pub get &&
-flutter pub run build_runner build &&
+flutter pub run build_runner build --delete-conflicting-outputs &&
 flutter pub run intl_utils:generate &&
 flutter build apk --flavor=sc
