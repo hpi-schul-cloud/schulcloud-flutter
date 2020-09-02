9d64a7faf05ca8236c718c0440be14f86cd76a98
Marcel Garus <marcel.garus@gmail.com>
2020-09-02 14:09:11 +0200

    Make syntax for bash more beautiful
    Sadly, this is not possible in bat

--- build-apk.sh
+++ build-apk.sh
@@ -1,1 +1,4 @@
-flutter pub get && flutter pub run build_runner build && flutter pub run intl_utils:generate && flutter build apk --flavor=sc
+flutter pub get &&
+flutter pub run build_runner build &&
+flutter pub run intl_utils:generate &&
+flutter build apk --flavor=sc
