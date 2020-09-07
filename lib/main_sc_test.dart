f06a9e318c93e1f4908e6865365ba044c14fb6c8
Jonas Wanke <contact@jonas-wanke.com>
2020-09-07 12:25:57 +0200

    fix: update sc_test flavor to use custom name

--- lib/main_sc_test.dart
+++ lib/main_sc_test.dart
@@ -3,6 +3,7 @@ import 'package:schulcloud/main.dart' as core;
 import 'main_sc.dart';
 
 final scTestAppConfig = scAppConfig.copyWith(
+  name: 'sc_test',
   host: 'test.hpi-schul-cloud.org',
   title: 'HPI Schul-Cloud (Test)',
 );
