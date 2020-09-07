bf239523a8778c224533dd443980bbde1079ff16
Jonas Wanke <contact@jonas-wanke.com>
2020-09-07 07:56:08 +0200

    feat(news): use SortFilterPage for NewsPage

--- lib/news/pages/news.dart
+++ lib/news/pages/news.dart
@@ -6,38 +6,46 @@ import '../widgets/article_card.dart';
 
 /// A page that displays a list of articles.
 class NewsPage extends StatelessWidget {
+  const NewsPage({this.sortFilterSelection});
+
+  static final sortFilterConfig = SortFilter<Article>(
+    sorters: {
+      'name': Sorter<Article>.name(
+        (s) => s.general_entity_property_name,
+        selector: (article) => article.title,
+      ),
+      'createdAt': Sorter<Article>.simple(
+        (s) => s.general_entity_property_createdAt,
+        selector: (article) => article.createdAt,
+      ),
+      'publishedAt': Sorter<Article>.simple(
+        (s) => s.general_entity_property_publishedAt,
+        selector: (article) => article.publishedAt,
+      ),
+    },
+    defaultSorter: 'publishedAt',
+    defaultSortOrder: SortOrder.descending,
+  );
+  final SortFilterSelection<Article> sortFilterSelection;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CollectionBuilder.populated<Article>(
-        collection: services.storage.root.news,
-        builder: handleLoadingErrorRefreshEmpty(
-          appBar: FancyAppBar(title: Text(context.s.news)),
-          emptyStateBuilder: (context) =>
-              EmptyStatePage(text: context.s.news_empty),
-          builder: (context, unsortedArticles, isFetching) {
-            final articles = unsortedArticles.toList()
-              ..sort((a1, a2) => a2.publishedAt.compareTo(a1.publishedAt));
-
-            return CustomScrollView(
-              slivers: <Widget>[
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (_, index) {
-                      return Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                        child: ArticleCard(articles[index].id),
-                      );
-                    },
-                    childCount: articles.length,
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
+    return SortFilterPage<Article>(
+      config: sortFilterConfig,
+      initialSelection: sortFilterSelection,
+      collection: services.storage.root.news,
+      appBarBuilder: (context, showSortFilterSheet) => FancyAppBar(
+        title: Text(context.s.news),
+        actions: <Widget>[SortFilterIconButton(showSortFilterSheet)],
       ),
+      emptyStateTextGetter: (s) => s.news_newsPage_empty,
+      filteredEmptyStateTextGetter: (s) => s.news_newsPage_emptyFiltered,
+      builder: (_, article, __, ___) {
+        return Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: ArticleCard(article.id),
+        );
+      },
     );
   }
 }
