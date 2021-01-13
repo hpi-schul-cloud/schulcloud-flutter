dce38a21b60ee46748c278f48ae2162dbf183bf7
Jonas Wanke <contact@jonas-wanke.com>
2021-01-13 17:31:09 +0100

    build: update sentry

--- lib/app/error_reporting.dart
+++ lib/app/error_reporting.dart
@@ -1,48 +1,21 @@
 import 'dart:async';
-import 'dart:io';
-import 'dart:ui';
 
-import 'package:device_info/device_info.dart';
-import 'package:flutter/foundation.dart';
-import 'package:hive_cache/hive_cache.dart';
 import 'package:logger/logger.dart';
-import 'package:schulcloud/brand/brand.dart';
 import 'package:sentry/sentry.dart' hide User;
-import 'package:system_info/system_info.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
-import 'logger.dart';
 import 'services.dart';
 import 'services/storage.dart';
 
-final _sentry = SentryClient(
-    dsn: 'https://2d9dda495b8f4626b01e28e24c19a9b5@sentry.schul-cloud.dev/7');
+const _sentryDsn =
+    'https://2d9dda495b8f4626b01e28e24c19a9b5@sentry.schul-cloud.dev/7';
 
 Future<void> runWithErrorReporting(Future<void> Function() body) async {
-  await runZonedGuarded<Future<void>>(
-    () async {
-      FlutterError.onError = (details) async {
-        if (isInDebugMode) {
-          FlutterError.dumpErrorToConsole(details);
-        }
-        await reportEvent(Event(
-          exception: details.exception,
-          stackTrace: details.stack,
-          tags: {'source': 'flutter'},
-        ));
-      };
-
-      await body();
+  await SentryFlutter.init(
+    (options) async {
+      if (await _enableSentry()) options.dsn = _sentryDsn;
     },
-    (error, stackTrace) async {
-      if (isInDebugMode) {
-        logger.e('Uncaught exception', error, stackTrace);
-      }
-      await reportEvent(Event(
-        exception: error,
-        stackTrace: stackTrace,
-        tags: {'source': 'dart'},
-      ));
-    },
+    appRunner: body,
   );
 }
 
@@ -52,36 +25,8 @@ bool get isInDebugMode {
   return inDebugMode;
 }
 
-Future<void> reportLogEvent(
-  Level level,
-  dynamic message,
-  dynamic error,
-  StackTrace stackTrace,
-) async {
-  const _loggerLevelToSentryLevel = {
-    Level.verbose: SeverityLevel.debug,
-    Level.debug: SeverityLevel.debug,
-    Level.info: SeverityLevel.info,
-    Level.warning: SeverityLevel.warning,
-    Level.error: SeverityLevel.error,
-    Level.wtf: SeverityLevel.fatal,
-    Level.nothing: SeverityLevel.debug,
-  };
-
-  final event = Event(
-    level: _loggerLevelToSentryLevel[level],
-    message: message?.toString(),
-    exception: error,
-    stackTrace: stackTrace,
-    tags: {'source': 'logger'},
-  );
-  await reportEvent(event);
-}
-
-Future<bool> reportEvent(Event event) async {
-  if (isInDebugMode) {
-    return true;
-  }
+Future<bool> _enableSentry() async {
+  if (isInDebugMode) return false;
 
   StorageService storage;
   try {
@@ -97,113 +42,31 @@ Future<bool> reportEvent(Event event) async {
   // We don't have the permission to report errors or storage is null and we
   // have a Schrödinger's cat situation in which we can't guarantee a permission
   // to do so.
-  if (storage == null || !storage.errorReportingEnabled.getValue()) {
-    return true;
-  }
-
-  final platformString = defaultTargetPlatform.toString();
-  final user = HiveCache.isInitialized ? await storage.userFromCache : null;
-  final fullEvent = Event(
-    release: services.packageInfo.version,
-    environment: isInDebugMode ? 'debug' : 'production',
-    message: event.message,
-    exception: event.exception,
-    stackTrace: event.stackTrace,
-    level: event.level,
-    contexts: await _getContexts(),
-    tags: {
-      'platform': platformString.substring(platformString.indexOf('.') + 1),
-      'flavor': services.config.name,
-      if (user != null) 'schoolId': user.schoolId,
-      for (final entry in event.tags?.entries ?? []) entry.key: entry.value,
-    },
-    extra: {
-      'locale': window.locale.toString(),
-      'textScaleFactor': window.textScaleFactor,
-    },
-  );
-
-  final result = await _sentry.capture(event: fullEvent);
-  return result.isSuccessful;
+  return storage != null && storage.errorReportingEnabled.getValue();
 }
 
-Future<Contexts> _getContexts() async {
-  try {
-    final packageInfo = services.packageInfo;
-    final app = App(
-      name: packageInfo.appName,
-      version: packageInfo.version,
-      identifier: packageInfo.packageName,
-      build: packageInfo.buildNumber,
-    );
-    final dartRuntime = Runtime(
-      name: 'Dart',
-      version: Platform.version,
-    );
+Future<void> reportLogEvent(
+  Level level,
+  dynamic message,
+  dynamic error,
+  StackTrace stackTrace,
+) async {
+  const _loggerLevelToSentryLevel = {
+    Level.verbose: SentryLevel.debug,
+    Level.debug: SentryLevel.debug,
+    Level.info: SentryLevel.info,
+    Level.warning: SentryLevel.warning,
+    Level.error: SentryLevel.error,
+    Level.wtf: SentryLevel.fatal,
+    Level.nothing: SentryLevel.debug,
+  };
 
-    if (Platform.isAndroid) {
-      final deviceInfo = await DeviceInfoPlugin().androidInfo;
-      return Contexts(
-        device: Device(
-          model: deviceInfo.model,
-          arch: SysInfo.kernelArchitecture,
-          manufacturer: deviceInfo.manufacturer,
-          brand: deviceInfo.brand,
-          simulator: !deviceInfo.isPhysicalDevice,
-          memorySize: SysInfo.getTotalPhysicalMemory(),
-          freeMemory: SysInfo.getFreePhysicalMemory(),
-        ),
-        operatingSystem: OperatingSystem(
-          name: Platform.operatingSystem,
-          version: deviceInfo.version.release,
-          build: deviceInfo.version.incremental,
-          kernelVersion: SysInfo.kernelVersion,
-          rawDescription:
-              'SDK ${deviceInfo.version.sdkInt}, ${Platform.operatingSystemVersion}',
-        ),
-        app: app,
-        runtimes: [dartRuntime],
-      );
-    } else if (Platform.isIOS) {
-      final deviceInfo = await DeviceInfoPlugin().iosInfo;
-      return Contexts(
-        device: Device(
-          name: deviceInfo.name,
-          family: deviceInfo.utsname.machine,
-          model: deviceInfo.model,
-          arch: SysInfo.kernelArchitecture,
-          manufacturer: 'Apple',
-          brand: 'Apple',
-          simulator: !deviceInfo.isPhysicalDevice,
-          memorySize: SysInfo.getTotalPhysicalMemory(),
-          freeMemory: SysInfo.getFreePhysicalMemory(),
-        ),
-        operatingSystem: OperatingSystem(
-          name: deviceInfo.systemName,
-          version: deviceInfo.systemVersion,
-          kernelVersion: SysInfo.kernelVersion,
-          rawDescription: Platform.operatingSystem,
-        ),
-        app: app,
-        runtimes: [dartRuntime],
-      );
-    } else {
-      return Contexts(
-        device: Device(
-          arch: SysInfo.kernelArchitecture,
-          memorySize: SysInfo.getTotalPhysicalMemory(),
-          freeMemory: SysInfo.getFreePhysicalMemory(),
-        ),
-        operatingSystem: OperatingSystem(
-          name: Platform.operatingSystem,
-          version: Platform.operatingSystemVersion,
-          kernelVersion: SysInfo.kernelVersion,
-        ),
-        app: app,
-        runtimes: [dartRuntime],
-      );
-    }
-  } catch (e) {
-    return null;
-  }
+  final event = SentryEvent(
+    level: _loggerLevelToSentryLevel[level],
+    message: Message(message?.toString()),
+    exception: error,
+    // stackTrace: stackTrace,
+    tags: {'source': 'logger'},
+  );
+  await Sentry.captureEvent(event, stackTrace: stackTrace);
 }
